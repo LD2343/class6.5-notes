0aaa829f8ca88a10c988a5219d2bc2041965199e
aaron-dm-mcdonald <mcdonald.dm.aaron@gmail.com>
2025-05-24 11:12:08 -0700

    add proxy subnet

--- 052425/3-subnets.tf
+++ 052425/3-subnets.tf
@@ -11,7 +11,7 @@ resource "google_compute_subnetwork" "hqinternal" {
 resource "google_compute_subnetwork" "regional_proxy_subnet" {
   name          = "regional-proxy-subnet"
   region        = "us-central1"
-  ip_cidr_range = "10.0.0.0/24"
+  ip_cidr_range = "10.100.0.0/24"
   purpose       = "REGIONAL_MANAGED_PROXY"
   network       = google_compute_network.main.id
   role          = "ACTIVE"
